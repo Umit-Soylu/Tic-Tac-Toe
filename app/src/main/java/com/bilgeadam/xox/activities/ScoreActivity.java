3d8c6fa0be948e5241409548e8e0cfbdf85f80ee
Umit-Soylu <umit.soylu@alteryx.com>
2021-04-03 16:10:27 +0300

    Test update

--- app/src/main/java/com/bilgeadam/xox/activities/ScoreActivity.java
+++ app/src/main/java/com/bilgeadam/xox/activities/ScoreActivity.java
@@ -1,6 +1,11 @@
 package com.bilgeadam.xox.activities;
 
+import android.view.View;
 import androidx.fragment.app.FragmentActivity;
 
 public class ScoreActivity extends FragmentActivity {
+
+    public void testMethod(View view){
+
+    }
 }
